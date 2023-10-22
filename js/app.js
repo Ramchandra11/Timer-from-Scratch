13275c7af1ebe69a72930f6cc97e3c9c284ae04a
Ramchandra11 <109614326+Ramchandra11@users.noreply.github.com>
2023-10-22 08:16:05 +0545

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const endDate ="23 October 2023 08:14"
+document.getElementById("end-date").innerText =endDate;
+const inputs =document.querySelectorAll("input")
+function clock(){
+    const end = new Date(endDate)
+    const now =new Date()
+    const diff =(end-now)/1000;
+   if (diff <0) return ;
+//    Convert into days
+   inputs[0].value=Math.floor(diff/3600/24);
+//    convert into hours
+   inputs[1].value=Math.floor(diff/60)%24;
+//    convert into minutes
+inputs[2].value=Math.floor(diff/60)%60;
+// console.log(Math.floor(diff/60)%60);
+// convert seconds
+inputs[3].value=Math.floor(diff%60);
+}
+// initial call
+clock()
+/*
+1 day 24 hr
+1 hr 60 min
+60 min 3600 sec
+
+
+
+
+*/
+setInterval(
+    ()=> {
+        clock()
+    },
+    1000
+)
